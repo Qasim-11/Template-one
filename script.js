04cb4e3729beb997d9b496f059e6a962d14a85b7
Qasim <119961870+Qasim-11@users.noreply.github.com>
2023-02-09 12:04:32 +0000

    الحمدلله

---
+++ script.js
@@ -0,0 +1,23 @@
+let List = document.getElementById("List")
+let s1 = document.getElementById("s1")
+let s3 = document.getElementById("s3")
+let s1Width = s1.style.width;
+let s3Width = s3.style.width;
+function DisplayList() {
+
+  if (List.style.display === "none" || List.style.display === "") {
+    List.style.display = "block";
+  } else {
+    List.style.display = "none";
+  }
+  if (s1.style.width === "60%") {
+    s1.style.width = s1Width;
+    s3.style.width = s3Width;
+  } else {
+    s1.style.width = "60%";
+    s3.style.width = "100%";
+  }
+
+}
+  
+
